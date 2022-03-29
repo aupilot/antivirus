bea6be786e775ff5999f9bd00bd25cd2ccfe63a8
kir <info@webage.net.au>
2022-03-30 07:11:38 +1000

    fix in docker/score parameters

--- run_score.sh
+++ run_score.sh
@@ -7,6 +7,6 @@ cd data/
 # docker login -u 8kir8
 # pwd: Supplier1
 #source /home/kir/Apps/alphafold/venv/bin/activate
-docker run -t --gpus all -v $(pwd):/workdir --workdir /workdir molecule:0329.2 /usr/bin/python3 /opt/dock-n-score.py $1 $2
+docker run -t --gpus all -v $(pwd):/workdir --workdir /workdir 8kir8/molecule:0329.2 /usr/bin/python3 /opt/dock-n-score.py $1 $2
 #docker ps
 
