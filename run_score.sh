9474f472f0515d1634bb98d9641f929f3f6ac745
kir <info@webage.net.au>
2022-07-02 11:22:07 +1000

    nc

--- run_score.sh
+++ run_score.sh
@@ -11,8 +11,8 @@ cd data/
 # pwd: Supplier1
 docker run -t --rm --gpus all -v $(pwd):/workdir --workdir /workdir 8kir8/score:latest /usr/bin/python3 /opt/dock-n-score.py $1 $2 $3 $4 $5
 # Dmytro's
-#docker run -t --rm --gpus '"device=0"' -v $(pwd):/workdir --workdir /workdir 8kir8/molecule:0605.1 /usr/bin/python3 /opt/dock-n-score.py $1 $2 $3 $4 $5
-#docker run -t --rm --gpus '"device=1"' -v $(pwd):/workdir --workdir /workdir 8kir8/molecule:0605.1 /usr/bin/python3 /opt/dock-n-score.py $1 $2 $3 $4 $5
+#docker run -t --rm --gpus '"device=0"' -v $(pwd):/workdir --workdir /workdir 8kir8/score:latest /usr/bin/python3 /opt/dock-n-score.py $1 $2 $3 $4 $5
+#docker run -t --rm --gpus '"device=1"' -v $(pwd):/workdir --workdir /workdir 8kir8/score:latest /usr/bin/python3 /opt/dock-n-score.py $1 $2 $3 $4 $5
 
 # inside the container
 #/usr/bin/python3 /opt/dock-n-score.py /workdir/best_07.pdb /workdir/7cr5_SPIKE.pdb 0.06 1
